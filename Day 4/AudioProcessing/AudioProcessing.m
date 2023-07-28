dadb664130c813bbc9fda0e3e02e539d8a9c3114
paragmraw <paragm.raw@gmail.com>
2023-07-28 12:42:54 +0530

    d4-c2

--- Day 4/AudioProcessing/AudioProcessing.m
+++ Day 4/AudioProcessing/AudioProcessing.m
@@ -1,9 +1,29 @@
 clear all; close all; clc;
+
+%% Record the Audio
 recorder = audiorecorder;
 disp("Start speaking now: ")
-recordblocking(recorder, 5)
+recordblocking(recorder, 20)
 disp("End of the recording")
+
+%% Visualizing the Spectrum
 play(recorder)
 myaudio = getaudiodata(recorder)
 sa = dsp.SpectrumAnalyzer
 step(sa, myaudio)
+
+%% Design the HPF
+Fs = 44.1e3;
+filtertype = 'FIR';
+Fpass=12e3;
+Fstop=8e3;
+Rp=0.1;
+Astop=80;
+FIRHPF=dsp.HighpassFilter('SampleRate',Fs, ...
+    'FilterType',filtertype, ...
+    'PassbandFrequency',Fpass, ...
+    'StopbandFrequency',Fstop, ...
+    'PassbandRipple',Rp, ...
+    'StopbandAttenuation',Astop);
+
+%% Filter the audio signal
